547787bbb8d5d66afd9b0ac7bf4b0a2cbce4d095
Faris Nour <farisnour93@gmail.com>
2019-10-18 22:12:38 -0400

    Added 3 models for image detection

---
+++ LogisticRegression.py
@@ -0,0 +1,49 @@
+import tensorflow as tf
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.optimizers import RMSprop
+
+batch_size = 128
+epochs = 1
+num_classes = 10
+input_dim = 28 * 28
+
+# the data, split between train and test sets
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train = x_train.reshape(60000, input_dim)
+x_test = x_test.reshape(10000, input_dim)
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+model = Sequential()
+model.add(Dense(num_classes, activation='softmax', input_shape=(input_dim,)))
+model.add(Dropout(0.95))
+
+model.summary()
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='sgd',
+              metrics=['accuracy'])
+
+history = model.fit(x_train, y_train,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    validation_data=(x_test, y_test))
+score = model.evaluate(x_train, y_train, verbose=0)
+print('Training loss: %.4f' % score[0])
+print('Training accuracy: %.4f' % score[1])
+print('')
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss: %.4f' % score[0])
+print('Test accuracy: %.4f' % score[1])
